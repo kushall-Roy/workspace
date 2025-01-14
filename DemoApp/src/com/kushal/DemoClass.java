e5819af533ec9918598a96d654c15fa3061d305d
kushall-Roy <roykushal1996@gmail.com>
2025-01-15 01:18:14 +0530

    First Commit

---
+++ DemoApp/src/com/kushal/DemoClass.java
@@ -0,0 +1,16 @@
+package com.kushal;
+
+public class DemoClass {
+
+	public static void main(String[] args) {
+		int[] a = {10,20,30,40};
+		
+		for(int ele : a) {
+			System.out.println(ele);
+		}
+		
+		String name = "Sachin";
+		int count = name.toUpperCase().length();
+		System.out.println("The length of the String is :: "+count);
+	}
+}
